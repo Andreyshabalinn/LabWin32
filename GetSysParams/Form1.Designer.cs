3454cef422d2decf1ba30852ba80fe9f59391861
Andrey <Andrey@DESKTOP-0TS8ORG>
2021-09-23 20:16:28 +0600

    Первый коммит

---
+++ GetSysParams/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace GetSysParams
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(2, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(265, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Имя ПК";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(2, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(263, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Имя пользователя";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(2, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(263, 19);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Системная директория";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(2, 98);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(265, 27);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Версия ОС";
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(2, 125);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(265, 21);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Тип загрузки";
+            // 
+            // label6
+            // 
+            this.label6.Location = new System.Drawing.Point(2, 162);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(265, 22);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Разрешение монитора";
+            // 
+            // label7
+            // 
+            this.label7.Location = new System.Drawing.Point(2, 197);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(733, 18);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Путь к картинке для рабочего стола = ";
+            // 
+            // label8
+            // 
+            this.label8.Location = new System.Drawing.Point(2, 235);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(263, 19);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Контрастность: ";
+            // 
+            // label9
+            // 
+            this.label9.Location = new System.Drawing.Point(2, 268);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(265, 18);
+            this.label9.TabIndex = 7;
+            this.label9.Text = "Цвет рабочего стола: ";
+            // 
+            // label10
+            // 
+            this.label10.Location = new System.Drawing.Point(2, 301);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(265, 21);
+            this.label10.TabIndex = 8;
+            this.label10.Text = "Дата :";
+            // 
+            // label11
+            // 
+            this.label11.Location = new System.Drawing.Point(2, 339);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(78, 17);
+            this.label11.TabIndex = 9;
+            this.label11.Text = "Цвет рабочего стола";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(33, 385);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Определить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(151, 338);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(31, 20);
+            this.textBox1.TabIndex = 11;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(203, 338);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(31, 20);
+            this.textBox2.TabIndex = 12;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(262, 338);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(31, 20);
+            this.textBox3.TabIndex = 13;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(159, 385);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Поменять цвет";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(483, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Win32";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
